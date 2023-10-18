38ffd9d3aaaf84b355d4c7fbb0f27cab58c6c06d
magall <rafitogm2@gmail.com>
2023-10-17 23:59:35 -0300

    [feat] Create input component

---
+++ react-challenge/src/components/Core/Inputs.tsx
@@ -0,0 +1,29 @@
+import { ChangeEventHandler } from "react";
+import { styled } from "styled-components";
+interface iInputText {
+    label: string;
+    name: string;
+    onChangeText: ChangeEventHandler<HTMLInputElement>;
+}
+
+const InputWrapper = styled.div`
+    display: flex;
+    padding: 8px;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+const Input = styled.input`
+    width: 150px;
+    border-radius: 4px;
+    border-color: transparent;
+`;
+
+export function InputText(props: iInputText) {
+    return (
+        <InputWrapper>
+            <label htmlFor={props.name} >{props.label}</label>
+            <Input type="text" name={props.name} onChange={props.onChangeText} />
+        </InputWrapper>
+    );
+}
