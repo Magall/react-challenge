2b47596feef0a8341a1db17b8f2ef138490b372e
magall <rafitogm2@gmail.com>
2023-10-18 00:01:16 -0300

    [feat] Button and grid components

---
+++ react-challenge/src/components/Core/Grids.tsx
@@ -0,0 +1,22 @@
+import { styled } from "styled-components"
+
+
+
+export const Horizontal = styled.div`
+    display:flex;
+    
+    
+`
+
+export const CenteredHorizontal = styled(Horizontal)`
+    justify-content:center;
+`;
+
+
+export const Vertical = styled.div`
+    display:flex;
+    flex-direction:column;
+`;
+export const CenteredVertical = styled(Vertical)`
+    align-items:center;
+`
